97e52d9f5379156103a5d92a42f6638ceee73bd1
CPrasa <chathuryaprasad@gmail.com>
2025-03-17 02:22:00 +0530

    successfully save data

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
-# This function get real time crypto currency price every day at 8AM
-# this also save the data as csv
-# It identify top 10 crypto and send mails
-
 import requests
 import pandas as pd
 from datetime import datetime
@@ -9,8 +5,8 @@ from datetime import datetime
 # API information
 url = 'https://api.coingecko.com/api/v3/coins/markets'
 param = {
-    'vs_currency' : 'usd',
-    'order' : 'market_cap_desc',
+    'vs_currency': 'usd',
+    'order': 'market_cap_desc',
     'per_page': 250,
     'page': 1
 }
@@ -19,7 +15,7 @@ param = {
 response = requests.get(url, params=param)
 
 if response.status_code == 200:
-    print('Connection Successfull! \nGetting the data...')
+    print('Connection Successful! \nGetting the data...')
     
     # storing the response into data
     data = response.json()
@@ -27,38 +23,32 @@ if response.status_code == 200:
     # creating df dataframe
     df = pd.DataFrame(data)
     
-    # print(df.columns)
-    # print(df.head())
+    # Selecting required columns
     df = df[[
-        'id','current_price', 'market_cap', 'price_change_percentage_24h',
+        'id', 'current_price', 'market_cap', 'price_change_percentage_24h',
         'ath', 'atl'
     ]]
     
-    #creating new columns
-    today =  datetime.now().strftime('%d-%m-%Y %S:%M:%H')
+    # creating new columns for the timestamp
+    today = datetime.now().strftime('%d-%m-%Y %H-%M-%S')  # Corrected the datetime format
     df['time_stamp'] = today
     
-    # getting top 10
+    # Get the top 10 negative price changes
     top_negative = df.sort_values(by='price_change_percentage_24h', ascending=True)
     top_negative_10 = top_negative.head(10)
-    top_negative_10.to_csv(f'top_negative_10 of {today} .csv', index=False)
+    top_negative_10.to_csv(f'top_negative_10_{today}.csv', index=False)
     
-    # positive top
+    # Get the top 10 positive price changes
     top_positive = df.sort_values(by='price_change_percentage_24h', ascending=False)
     top_positive_10 = top_positive.head(10)
-    top_positive_10.to_csv(f'top_positive_10 of {today} .csv', index=False)
+    top_positive_10.to_csv(f'top_positive_10_{today}.csv', index=False)
     
+    # Saving the overall data to CSV
+    df.to_csv(f'crypto_data_{today}.csv', index=False)
     
-    # saving the data
-    df.to_csv(f'crypto_data {today}.csv', index=False)
     print(f'Top 10 crypto with highest price decrease % {today} \n{top_negative_10}')
     print(f'Top 10 crypto with highest price increase % {today} \n{top_positive_10}')
-    
-    print("Data saved successfull!")
+    print("Data saved successfully!")
     
 else:
-    print(f"Connection Failed Error Code {response.status_code}")
-    
-
-
-
+    print(f"Connection Failed. Error Code {response.status_code}")
